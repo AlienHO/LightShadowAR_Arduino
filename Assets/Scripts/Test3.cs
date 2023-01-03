5582ea542a964404f19a596e997ae02a8a874043
Rain <2215338912@qq.com>
2023-01-03 21:25:32 +0800

    master测试

--- Assets/Scripts/Test3.cs
+++ Assets/Scripts/Test3.cs
@@ -2,23 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test2 : MonoBehaviour
+public class Test3 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("第一次测试");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-
-    void Test()
-    {
-        //
     }
 }
